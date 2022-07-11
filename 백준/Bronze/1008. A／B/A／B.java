8f25e20bd5333dd27fb136e764f1ec521f4c0e5f
SMJminjeong <94827896+SMJminjeong@users.noreply.github.com>
2022-07-12 00:19:26 +0900

    [Bronze V] Title: A/B, Time: 212 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        double A = scan.nextInt();
+        double B = scan.nextInt();
+
+        double result = A/B;
+        System.out.println(result);
+    }
+}
