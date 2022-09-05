3a0ac3eddc0e1cbc13b5ee86da59e9a2f2655eae
SMJminjeong <sdwp0920@gmail.com>
2022-09-05 23:02:55 +0900

    [Bronze III] Title: 오븐 시계, Time: 228 ms, Memory: 18404 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
+
+       int min = 60*A+B;
+       min += C;
+
+       int hour = (min/60)%24;
+       int minu = min%60;
+
+        System.out.println(hour + " " + minu);
+    }
+
+}
