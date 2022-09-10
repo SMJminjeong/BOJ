9f49fccff0c825102473aad8dbb5b4e703f7aa7f
SMJminjeong <sdwp0920@gmail.com>
2022-09-11 00:31:39 +0900

    [Bronze V] Title: 영수증, Time: 224 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         int X = scanner.nextInt();
         int N = scanner.nextInt();
-        int order = 0;
-        
-        for(int i=1; i<=N; i++){
+
+        for(int i=0; i<N; i++){
             int a = scanner.nextInt();
             int b = scanner.nextInt();
-            order += a*b;
+
+            X = X-(a*b);
         }
-        if(X==order){
+        if(X==0){
             System.out.println("Yes");
         }else {
             System.out.println("No");
         }
-
     }
 }
