74ee4e0f59d2e2e905d3fa7b9da807fb42f6ede6
SMJminjeong <sdwp0920@gmail.com>
2022-09-11 00:05:49 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,16 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main{
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
 
-        int n = scanner.nextInt();
-        int result = 0;
-
-        for(int i = 1; i<=n; i++){
-            result += i;
-        }
-        System.out.println(result);
-
+        for(int i=1; i<=n; i++) sum+=i;
+        System.out.println(sum);
     }
 }
