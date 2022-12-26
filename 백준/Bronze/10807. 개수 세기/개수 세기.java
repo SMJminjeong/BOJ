6448561613e3aadd0b21256ce4d613b2dd75acbf
SMJminjeong <sdwp0920@gmail.com>
2022-12-26 21:23:07 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while(st.hasMoreTokens()){
+            int m = Integer.parseInt(st.nextToken());
+            map.put(m, map.getOrDefault(m,0)+1);
+        }
+        int v = Integer.parseInt(br.readLine());
+
+        if(map.containsKey(v)){
+            System.out.println(map.get(v));
+        }else{
+            System.out.println(0);
+        }
+    }
+}
