6e990174f3cb37049e1d9ae4bdfecca2d882f3e6
SMJminjeong <sdwp0920@gmail.com>
2023-01-08 08:37:50 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -2,12 +2,7 @@ class Solution {
     public int solution(int num1, int num2) {
         int answer = 0;
         if((0 <= num1 && num1 <= 10000) && (0 <= num2 && num2 <= 10000)){
-            if(num1 == num2){
-                answer = 1;
-            }
-            if(num1 != num2){
-                answer = -1;
-            }
+            answer = (num1==num2) ? 1 : -1;
         }
         return answer;
     }
