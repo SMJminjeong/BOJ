05594b618cfb25c117ea37b67bebb6bbcabb9eb0
SMJminjeong <sdwp0920@gmail.com>
2023-01-07 23:51:32 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,23 +1,16 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] student = new int[31];
-		
-		for(int i=1; i<29; i++) {
-			int success = sc.nextInt();
-			student[success] = 1;
-		}
-		for(int i=1; i<student.length; i++) {
-			if(student[i]!=1)
-				System.out.println(i);
-		}
-		
-		
-		sc.close();
-
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] arr = new boolean[31];
+        for (int i = 0; i < 28; i++) {
+            int n = Integer.parseInt(br.readLine());
+            arr[n] = true;
+        }
+        for (int i = 1; i <= 30; i++) {
+            if (!arr[i]) System.out.println(i);
+        }
+    }
 }
