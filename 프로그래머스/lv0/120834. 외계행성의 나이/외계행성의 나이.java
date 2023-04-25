52de8ed1d71c2ae3f8c57493e8c06039f6877d4b
SMJminjeong <sdwp0920@gmail.com>
2023-04-25 12:24:43 +0900

    [level 0] Title: 외계행성의 나이, Time: 12.39 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,14 +1,6 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
-        String ageToString = "abcdefghij";
-        
-        String[] ageArr = String.valueOf(age).split("");
-        
-        for(int i = 0; i < ageArr.length; i++){
-            answer += ageToString.charAt(Integer.valueOf(ageArr[i]));
-        }
-        
         return answer;
     }
 }
