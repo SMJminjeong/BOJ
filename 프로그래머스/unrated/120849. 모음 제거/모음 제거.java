d29078b792a5fb8a96dbf4fa3b7866d456aaecb2
SMJminjeong <sdwp0920@gmail.com>
2023-11-02 21:48:16 +0900

    [level unrated] Title: 모음 제거, Time: 0.15 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -1,12 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string;
-        String[] vowels = {"a", "e", "i", "o", "u"};
-        
-        for (String v : vowels) {
-            answer = answer.replaceAll(v, "");
-        }
-        
-        return answer;
+        return my_string.replaceAll("[aeiou]", "");
     }
 }
