15042d6cd7e138f8cda77bd202a2fd8493a50b42
SMJminjeong <sdwp0920@gmail.com>
2023-11-13 22:40:35 +0900

    [level unrated] Title: 삼각형의 완성조건 (1), Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        int max = 0;
+        int index = 0;
+        int sum = 0;
+        
+        for(int i = 0; i<sides.length; i++){
+            if(max < sides[i]) {
+                max = sides[i];
+                index = i;
+            }
+        }
+        for(int i = 0; i<sides.length; i++){
+            if(i != index){
+                sum += sides[i];
+            }
+        }
+        if(max < sum){
+            answer = 1;
+        }else{
+            answer = 2;
+        }
+        return answer;
+    }
+}
