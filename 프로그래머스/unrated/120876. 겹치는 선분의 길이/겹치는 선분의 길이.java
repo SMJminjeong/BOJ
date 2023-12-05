cf56ba806e44e0cb7b835e8c46cd603f2b061598
SMJminjeong <sdwp0920@gmail.com>
2023-12-05 10:53:51 +0900

    [level unrated] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/unrated/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -1,20 +1,34 @@
 class Solution {
-    public int solution(int[][] lines){
+
+    public int solution(int[][] lines) {
+
         int answer = 0;
-        int[] count = new int[201];
+        int temp = 0; 
+        for(int i = 0; i < lines.length; i++){
+            int startLine = lines[i][0];
+            int endLine = lines[i][1];
+            for(int j = i + 1; j < lines.length; j++){
+                int sndStartLine = lines[j][0];
+                int sndEndLine = lines[j][1];
 
-        for(int[] i : lines){
-            int start = i[0]+100;
-            int end = i[1]+100;
-
-            for(int k=start; k<end; k++){
-                count[k] += 1;
+                if(startLine == sndStartLine){
+                    temp = Math.min(endLine, sndEndLine);
+                }
+                else if(startLine < sndEndLine){ // 1시작점 < 2끝점
+                    temp = sndEndLine - startLine;
+                }
+                else if(sndStartLine < endLine){ // 2시작점 < 1끝점
+                    temp = endLine - sndStartLine;
+                }
+                System.out.println(temp);
+            }
+            if(answer < temp){
+                answer = temp;
             }
         }
 
-        for(int i : count){
-            if(i>1) answer++;
-        }
         return answer;
+
     }
+
 }
